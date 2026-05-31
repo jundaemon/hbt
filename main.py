aacf0e3c35d05b0ad4343e81a4ef8c1d62638bbc
Jun Wei <junweilee72@gmail.com>
2026-06-01 03:42:48 +0800

    variable name changes

--- main.py
+++ main.py
@@ -1,40 +1,36 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-PULSES = 500_000
-INTERVAL_NS = 50
+EXP_N = 500_000
+T_NS = 50
 
 
-def calc_detection_times(efficiency: float, lifetime_ns: int) -> np.ndarray:
-    emission_duration = np.log(np.random.rand(PULSES)) * -lifetime_ns
-    if efficiency == 1.0:
-        return (np.arange(0, PULSES) * INTERVAL_NS) + emission_duration
+def t_calc(eff: float, tau_ns: int) -> np.ndarray:
+    dur = np.log(np.random.rand(EXP_N)) * -tau_ns
+    if eff == 1.0:
+        return (np.arange(0, EXP_N) * T_NS) + dur
     else:
         return (
-            np.cumsum(
-                np.floor(np.log(np.random.rand(PULSES)) / np.log(1 - efficiency)) + 1
-            )
-            * INTERVAL_NS
-        ) + emission_duration
+            np.cumsum(np.floor(np.log(np.random.rand(EXP_N)) / np.log(1 - eff)) + 1)
+            * T_NS
+        ) + dur
 
 
-def split_detections(detection_times: np.ndarray) -> list[np.ndarray]:
-    split_mask = np.random.rand(len(detection_times)) <= 0.5
-    return [detection_times[split_mask], detection_times[~split_mask]]
+def t_split(set_t: np.ndarray) -> list[np.ndarray]:
+    mask = np.random.rand(len(set_t)) <= 0.5
+    return [set_t[mask], set_t[~mask]]
 
 
-def calc_taus(detectors: list[np.ndarray], half_window_ns: int) -> np.ndarray:
-    starts = np.searchsorted(detectors[1], detectors[0] - half_window_ns, side="left")
-    ends = np.searchsorted(detectors[1], detectors[0] + half_window_ns, side="right")
+def tau_calc(sets_t: list[np.ndarray], half_window_ns: int) -> np.ndarray:
+    starts = np.searchsorted(sets_t[1], sets_t[0] - half_window_ns, side="left")
+    ends = np.searchsorted(sets_t[1], sets_t[0] + half_window_ns, side="right")
     ranges = ends - starts
 
     taus = np.empty(ranges.sum())
-    taus_curr = 0
-    for i, t_1 in enumerate(detectors[0]):
-        taus[taus_curr : taus_curr + ranges[i]] = (
-            t_1 - detectors[1][starts[i] : ends[i]]
-        )
-        taus_curr += ranges[i]
+    taus_i = 0
+    for i, t_1 in enumerate(sets_t[0]):
+        taus[taus_i : taus_i + ranges[i]] = t_1 - sets_t[1][starts[i] : ends[i]]
+        taus_i += ranges[i]
 
     return taus
 
@@ -48,10 +44,10 @@ def plot_coincidence_events(taus: np.ndarray, bins: int) -> None:
 
 
 if __name__ == "__main__":
-    first_emitter_times = calc_detection_times(0.9, 3)
-    second_emitter_times = calc_detection_times(0.9, 3)
-    detection_times = np.sort(np.concat([first_emitter_times, second_emitter_times]))
-    detectors = split_detections(detection_times)
-    taus = calc_taus(detectors, 1000)
+    set_t_1 = t_calc(0.1, 3)
+    set_t_2 = t_calc(0.1, 3)
+    set_t = np.sort(np.concat([set_t_1, set_t_2]))
+    sets_t = t_split(set_t)
+    taus = tau_calc(sets_t, 1000)
 
     plot_coincidence_events(taus, 10000)
