d271735cd5c842320504a3fd5e38205a82e511d3
Jun Wei <junweilee72@gmail.com>
2026-06-04 20:07:54 +0800

    prelim results

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import math
+import sys
 import time
 
 import numpy as np
+import polars as pl
 from matplotlib import pyplot as plt
 
 
 def t_calc(
-    rng: np.random.Generator, exp_N: int, T_ns: int, eff: float, lifetime_ns: int
+    rng: np.random.Generator, exp_N: int, T_ns: float, eff: float, lifetime_ns: float
 ) -> np.ndarray:
     dur = np.log(rng.random(exp_N)) * -lifetime_ns
     if eff == 1.0:
@@ -22,7 +24,7 @@ def t_split(rng: np.random.Generator, set_t: np.ndarray) -> list[np.ndarray]:
     return [set_t[mask], set_t[~mask]]
 
 
-def tau_calc(sets_t: list[np.ndarray], half_window_ns: int) -> np.ndarray:
+def tau_calc(sets_t: list[np.ndarray], half_window_ns: float) -> np.ndarray:
     starts = np.searchsorted(sets_t[1], sets_t[0] - half_window_ns, side="left")
     ranges = (
         np.searchsorted(sets_t[1], sets_t[0] + half_window_ns, side="right") - starts
@@ -33,14 +35,14 @@ def tau_calc(sets_t: list[np.ndarray], half_window_ns: int) -> np.ndarray:
         - sets_t[1][
             (
                 np.repeat(starts, ranges)
-                + np.arange(ranges.sum())
+                + np.arange(0, ranges.sum())
                 - np.repeat(np.cumsum(ranges) - ranges, ranges)
             )
         ]
     )
 
 
-def g2_zero_calc(taus: np.ndarray, T_ns: int, bins: int) -> np.float64:
+def g2_zero_calc(taus: np.ndarray, T_ns: float, bins: int) -> np.float64:
     hist, edges = np.histogram(taus, bins)
     bins_per_pulse = math.floor(T_ns / (edges[1] - edges[0]))
 
@@ -48,14 +50,12 @@ def g2_zero_calc(taus: np.ndarray, T_ns: int, bins: int) -> np.float64:
         np.where(
             (hist[1:-1] > hist[2:])
             & (hist[1:-1] > hist[:-2])
-            & (hist[1:-1] > hist.max() * 0.95)
+            & (hist[1:-1] > hist.max() * 0.9)
         )[0]
         + 1
     )
     side_crests_i = side_crests_i[
-        np.insert(
-            np.abs(np.diff(side_crests_i)) > math.floor(bins_per_pulse * 0.95), 0, True
-        )
+        np.insert(np.diff(side_crests_i) > math.floor(bins_per_pulse * 0.9), 0, True)
     ]
 
     mask = np.full(len(side_crests_i), True)
@@ -78,28 +78,68 @@ def plot_coincidence_events(taus: np.ndarray, bins: int) -> None:
     plt.show()
 
 
+EXP_N = 500_000
+T_NS = 50.0
+LIFETIME_NS = 3.0
+HALF_WINDOW_NS = 1_000.0
+BINS = 10_000
+SEED = 10
+RNG = np.random.default_rng(SEED)
+
+
+def run_generation() -> None:
+    effs = np.linspace(0.1, 1.0, 10, True)
+    data_points = len(effs) ** 2
+    results = {
+        "Seed used": np.full(data_points, SEED, np.int64),
+        "Lifetime (ns)": np.full(data_points, LIFETIME_NS, np.float64),
+        "Time between pulses (ns)": np.full(data_points, T_NS, np.float64),
+        "Efficiency of emitter 1": np.empty(data_points, np.float64),
+        "Efficiency of emitter 2": np.empty(data_points, np.float64),
+        "Detections at detector 1": np.empty(data_points, np.int64),
+        "Detections at detector 2": np.empty(data_points, np.int64),
+        "g2": np.empty(data_points, np.float64),
+    }
+
+    idx = 0
+    total_time_s = 0
+    print("Started calculations\n")
+
+    for eff_1 in effs:
+        for eff_2 in effs:
+            start = time.perf_counter()
+
+            set_t_1 = t_calc(RNG, EXP_N, T_NS, eff_1, LIFETIME_NS)
+            set_t_2 = t_calc(RNG, EXP_N, T_NS, eff_2, LIFETIME_NS)
+
+            set_t = np.sort(np.concat([set_t_1, set_t_2]))
+            sets_t = t_split(RNG, set_t)
+
+            taus = tau_calc(sets_t, HALF_WINDOW_NS)
+            g2_zero = g2_zero_calc(taus, T_NS, BINS)
+
+            end = time.perf_counter()
+            print(f"Finished a calculation, time taken: {(end - start):.2f}s")
+            total_time_s += end - start
+
+            results["g2"][idx] = g2_zero
+            results["Efficiency of emitter 1"][idx] = eff_1
+            results["Efficiency of emitter 2"][idx] = eff_2
+            results["Detections at detector 1"][idx] = len(sets_t[0])
+            results["Detections at detector 2"][idx] = len(sets_t[1])
+
+            idx += 1
+
+    print(f"\nFinished {data_points} calculations")
+    print(f"Total time taken: {total_time_s:.2f}s")
+
+    pl.DataFrame(results).write_excel("results.xlsx", autofit=True)
+    print("\nExcel sheet generated")
+
+
 if __name__ == "__main__":
-    exp_N = 500_000
-    T_ns = 50
-    eff = 1
-    lifetime_ns = 3
-    half_window_ns = 1000
-    bins = 10000
-
-    rng = np.random.default_rng(10)
-
-    start = time.perf_counter()
-
-    set_t_1 = t_calc(rng, exp_N, T_ns, eff, lifetime_ns)
-    set_t_2 = t_calc(rng, exp_N, T_ns, eff, lifetime_ns)
-
-    set_t = np.sort(np.concat([set_t_1, set_t_2]))
-    sets_t = t_split(rng, set_t)
-
-    taus = tau_calc(sets_t, half_window_ns)
-    g2_zero = g2_zero_calc(taus, T_ns, bins)
-
-    end = time.perf_counter()
-
-    print(f"Calculated g2: {g2_zero}")
-    print(f"Time taken: {end - start}")
+    try:
+        run_generation()
+    except Exception as e:
+        print(e)
+        sys.exit(1)
